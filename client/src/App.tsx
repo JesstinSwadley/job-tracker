948eaef4137cbab4e0acd1cb6256d7c5dd00f05e
Jesstin Swadley <contact@jesstin.com>
2025-11-20 23:53:39 -0800

    put new job form in popup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,37 @@
+import { useState } from "react"
 import JobTable from "./components/JobTable"
 import NewJobForm from "./components/NewJobForm"
+import PopUp from "./components/PopUp";
 
 function App() {
+	const [showNewPopup, setShowNewPopup] = useState<boolean>(false);
+
+	const openNewForm = () => {
+		setShowNewPopup(true)
+	}
+
+	const closeNewPopUp = () => {
+		setShowNewPopup(false)
+	}
+	
 	return (
 		<>
-			<section 
-				className="flex justify-center m-5">
-				<NewJobForm />
-			</section>
+			<button 
+				className="mr-3 px-4 py-2 rounded bg-blue-600 text-zinc-100 font-semibold hover:bg-blue-700"
+				onClick={openNewForm}>
+					<span>New Job</span>
+			</button>
 
 			<section
 				className="flex justify-center m-5">
 				<JobTable />
 			</section>
+
+			<PopUp
+				showPopup={showNewPopup}
+				onClose={closeNewPopUp}>
+					<NewJobForm />
+			</PopUp>
 		</>
 	)
 }
