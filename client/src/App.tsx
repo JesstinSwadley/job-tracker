59371a8819dc426dc95569e5555a77df602c831f
Jesstin Swadley <contact@jesstin.com>
2025-12-28 00:03:15 -0800

    add react-router & Home Page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,45 +1,25 @@
-import { useState } from "react"
-import JobTable from "./components/JobTable"
-import NewJobForm from "./components/NewJobForm"
-import PopUp from "./components/PopUp";
-import RegisterForm from "./components/RegisterForm";
+
 import Nav from "./components/Nav";
+import { BrowserRouter, Routes } from "react-router";
+import { Route } from "react-router";
+import Home from "./pages/Home";
 
 function App() {
-	const [showNewPopup, setShowNewPopup] = useState<boolean>(false);
 
-	const openNewForm = () => {
-		setShowNewPopup(true)
-	}
-
-	const closeNewPopUp = () => {
-		setShowNewPopup(false)
-	}
 	
 	return (
 		<>
-			<Nav />
+			<BrowserRouter>
+				<Nav />
 
-			<button 
-				className="mr-3 px-4 py-2 rounded bg-blue-600 text-zinc-100 font-semibold hover:bg-blue-700"
-				onClick={openNewForm}>
-					<span>New Job</span>
-			</button>
-
-			<div>
-				<RegisterForm />
-			</div>
-
-			<section
-				className="flex justify-center m-5">
-				<JobTable />
-			</section>
-
-			<PopUp
-				showPopup={showNewPopup}
-				onClose={closeNewPopUp}>
-					<NewJobForm />
-			</PopUp>
+				<main>
+					<Routes>
+						<Route 
+							path="/"
+							Component={Home}/>
+					</Routes>
+				</main>
+			</BrowserRouter>
 		</>
 	)
 }
---
+++ client/src/pages/Home.tsx
@@ -0,0 +1,44 @@
+import { useState } from "react"
+import JobTable from "../components/JobTable"
+import NewJobForm from "../components/NewJobForm"
+import PopUp from "../components/PopUp";
+import RegisterForm from "../components/RegisterForm";
+
+const Home = () => {
+	const [showNewPopup, setShowNewPopup] = useState<boolean>(false);
+
+	const openNewForm = () => {
+		setShowNewPopup(true)
+	}
+
+	const closeNewPopUp = () => {
+		setShowNewPopup(false)
+	}
+
+	return (
+		<>
+			<button 
+				className="mr-3 px-4 py-2 rounded bg-blue-600 text-zinc-100 font-semibold hover:bg-blue-700"
+				onClick={openNewForm}>
+					<span>New Job</span>
+			</button>
+
+			<div>
+				<RegisterForm />
+			</div>
+
+			<section
+				className="flex justify-center m-5">
+				<JobTable />
+			</section>
+
+			<PopUp
+				showPopup={showNewPopup}
+				onClose={closeNewPopUp}>
+					<NewJobForm />
+			</PopUp>
+		</>
+	)
+}
+
+export default Home
