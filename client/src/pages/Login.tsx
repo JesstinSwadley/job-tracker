7672cb51c3419b53fa1f86f1932efb94c03c249e
Jesstin Swadley <contact@jesstin.com>
2026-01-06 00:08:25 -0800

    develop login form

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,8 +1,61 @@
 import React from 'react'
+import { Link } from 'react-router'
 
 const Login = () => {
 	return (
-		<div>Login</div>
+		<div>
+			<div>
+				<form action="">
+					<h2>Login</h2>
+					<Link
+						to="#">
+							Need to register an account? Create an account
+					</Link>
+					
+					<div>
+						<label 
+							htmlFor="emailusername">
+								Email or Username
+						</label>
+						<input 
+							type="text" 
+							name="emailusername" 
+							id="emailusername" />
+					</div>
+					<div>
+						<label 
+							htmlFor="password">
+								Password
+						</label>
+						<input 
+							type="password" 
+							name="password" 
+							id="password" />
+					</div>
+
+					<input 
+						type="checkbox" 
+						name="rememberMe" 
+						id="rememberMe" />
+					<label 
+						htmlFor="rememberMe">
+							Keep me logged in?
+					</label>
+
+					<button>Login</button>
+
+					<Link
+						to="#">
+							Forgot Username?
+					</Link>
+					
+					<Link
+						to="#">
+							Forgot Password?
+					</Link>
+				</form>
+			</div>
+		</div>
 	)
 }
 
