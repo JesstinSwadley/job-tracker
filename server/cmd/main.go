6f338272cd18e8abb898b807001b3bc518593939
Jesstin Swadley <contact@jesstin.com>
2026-02-11 01:48:58 -0800

    have main.go working again

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -7,22 +7,74 @@ import (
 	"os"
 	"time"
 
-	"github.com/rs/cors"
-
 	"github.com/JesstinSwadley/job-tracker/api"
 	"github.com/JesstinSwadley/job-tracker/api/handler"
 	"github.com/JesstinSwadley/job-tracker/internal/database"
 	"github.com/JesstinSwadley/job-tracker/internal/repository"
+	"github.com/rs/cors"
 )
 
+// import (
+// 	"context"
+// 	"log"
+// 	"net/http"
+// 	"os"
+// 	"time"
+
+// 	"github.com/rs/cors"
+
+// 	"github.com/JesstinSwadley/job-tracker/api"
+// 	"github.com/JesstinSwadley/job-tracker/api/handler"
+// 	"github.com/JesstinSwadley/job-tracker/internal/database"
+// 	"github.com/JesstinSwadley/job-tracker/internal/repository"
+// )
+
+// func main() {
+// 	// Server Port Lookup
+// 	port, _ := os.LookupEnv("PORT")
+// 	if port == "" {
+// 		port = "8080"
+// 		log.Println("PORT not set. Defaulting to 8080")
+// 	}
+
+// 	// Connect To Database
+// 	dbPool := database.DatabasePool()
+// 	defer dbPool.Close()
+
+// 	if os.Getenv("SKIP_DB_PING") != "1" {
+// 		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+// 		defer cancel()
+
+// 		if err := dbPool.Ping(ctx); err != nil {
+// 			log.Fatalf("failed to ping database: %v", err)
+// 		}
+// 	}
+
+// sqlcRepo := repository.New(dbPool)
+// jobRepo := &handler.SQLCJobRepo{Queries: sqlcRepo}
+
+// 	// API Router
+// 	apiHandler := api.ApiRouter(jobRepo)
+
+// 	// CORS Handler
+// 	handler := cors.AllowAll().Handler(apiHandler)
+
+// 	// Server
+// 	log.Println("server listening on PORT:" + port)
+// 	if err := http.ListenAndServe(":"+port, handler); err != nil {
+// 		log.Fatal(err)
+// 		os.Exit(1)
+// 	}
+// }
+
 func main() {
-	// Server Port Lookup
-	port, ok := os.LookupEnv("PORT")
-	if !ok {
-		panic("There is no server port defined")
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "8080"
+		log.Println("PORT not set. Defaulting to 8080")
 	}
 
-	// Connect To Database
 	dbPool := database.DatabasePool()
 	defer dbPool.Close()
 
@@ -38,16 +90,14 @@ func main() {
 	sqlcRepo := repository.New(dbPool)
 	jobRepo := &handler.SQLCJobRepo{Queries: sqlcRepo}
 
-	// API Router
 	apiHandler := api.ApiRouter(jobRepo)
 
-	// CORS Handler
 	handler := cors.AllowAll().Handler(apiHandler)
 
-	// Server
-	log.Println("server listening on PORT:" + port)
+	log.Printf("Server listening on PORT: %s", port)
+
 	if err := http.ListenAndServe(":"+port, handler); err != nil {
-		log.Fatal(err)
-		os.Exit(1)
+		log.Fatalf("Server failed: %v", err)
 	}
+
 }
