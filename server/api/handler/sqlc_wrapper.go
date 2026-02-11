9513169b6ee04e19b88708aad8c9f543a30bbd03
Jesstin Swadley <contact@jesstin.com>
2026-02-11 01:48:44 -0800

    remove unnecessary comments

--- server/api/handler/sqlc_wrapper.go
+++ server/api/handler/sqlc_wrapper.go
@@ -6,12 +6,10 @@ import (
 	"github.com/JesstinSwadley/job-tracker/internal/repository"
 )
 
-// SQLCJobRepo wraps *respository.Queries to implement minimal JobRepo interface
 type SQLCJobRepo struct {
 	Queries *repository.Queries
 }
 
-// InsertJob adapts SQLC InsertJob to minimal interface
 func (s *SQLCJobRepo) InsertJob(ctx context.Context, position, company string) (repository.Job, error) {
 	return s.Queries.InsertJob(ctx, repository.InsertJobParams{
 		Position: position,
