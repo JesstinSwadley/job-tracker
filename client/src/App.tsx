0eb072f97208007f07df150882d1b32d37c4f6e3
Jesstin Swadley <contact@jesstin.com>
2026-03-06 12:05:56 -0800

    add protected routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,6 +4,7 @@ import { Route } from "react-router";
 import Home from "./pages/Home";
 import Login from "./pages/auth/Login";
 import Register from "./pages/auth/Register";
+import ProtectedRoute from "./components/auth/ProtectedRoute";
 
 function App() {	
 	return (
@@ -24,6 +25,14 @@ function App() {
 						<Route
 							path="/register"
 							Component={Register} />
+
+						<Route 
+							element={<ProtectedRoute />}>
+
+							<Route 
+								path="/dashboard" 
+								element={<div className="p-10 text-2xl font-bold text-center">Your Job Tracker Dashboard</div>} />
+						</Route>
 					</Routes>
 				</main>
 			</BrowserRouter>
