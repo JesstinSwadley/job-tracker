e2836ba24732ddbf0925999fafc9604b1a0fcc22
Jesstin Swadley <contact@jesstin.com>
2026-04-04 22:27:17 -0700

    0.9.0

--- client/src/components/auth/ProtectedRoute.tsx
+++ client/src/components/auth/ProtectedRoute.tsx
@@ -1,7 +1,8 @@
-import { Navigate, Outlet } from "react-router";
+import { Navigate, Outlet, useLocation } from "react-router";
 
 const ProtectedRoute = () => {
 	const token = localStorage.getItem('token');
+	const location = useLocation();
 
 	if (!token) {
 		return <Navigate to={"/login"} replace />;
