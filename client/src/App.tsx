6808dab6455bc026d436cc8b9e4da9a84087f0c2
Jesstin Swadley <contact@jesstin.com>
2026-04-26 17:43:02 -0700

    Move Nav.tsx from components directory into layouts directory

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import Nav from "./components/Nav";
+import Nav from "./components/layouts/Nav";
 import { BrowserRouter } from "react-router";
 import AppRoutes from "./routes";
 import { Toaster } from "react-hot-toast";
