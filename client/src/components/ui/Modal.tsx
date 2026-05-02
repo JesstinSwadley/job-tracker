e2b8b46b048b49f48c3a2b1e0e23545515731ac2
Jesstin Swadley <contact@jesstin.com>
2026-05-01 18:25:47 -0700

    add additional const styles & comments, change Modal.tsx to use MODAL_SIZES

--- client/src/components/ui/Modal.tsx
+++ client/src/components/ui/Modal.tsx
@@ -1,6 +1,6 @@
 import { X } from "lucide-react";
 import { useEffect } from "react";
-import { SizeClasses } from "../../libs/constants";
+import { MODAL_SIZES } from "../../libs/constants";
 
 interface ModalProps {
 	isOpen: boolean;
@@ -35,7 +35,7 @@ const Modal = ({ isOpen, onClose, title, children, size = "md" }: ModalProps) =>
 			className="fixed inset-0 z-50 flex items-center justify-center bg-black/60 backdrop-blur-sm p-4 animate-in fade-in duration-200">
 				<div
 					onClick={(e) => e.stopPropagation()}
-					className={`w-full ${SizeClasses[size]} rounded-2xl bg-white p-8 shadow-2xl animate-in zoom-in-95 duration-200`}>
+					className={`w-full ${MODAL_SIZES[size]} rounded-2xl bg-white p-8 shadow-2xl animate-in zoom-in-95 duration-200`}>
 						<div
 							className="mb-6 flex items-center justify-between">
 								<h2 
--- client/src/libs/constants.ts
+++ client/src/libs/constants.ts
@@ -1,8 +1,26 @@
-export const SizeClasses = {
+/**
+ * Modal & Layout Sizes 
+ */
+export const MODAL_SIZES = {
 	sm: 'max-w-sm',
 	md: 'max-w-md',
 	lg: 'max-w-lg',
 	xl: 'max-w-2xl'
 };
 
+/**
+* Form Styles
+*/
 export const INPUT_STYLES = "w-full rounded-lg border-2 border-gray-100 bg-gray-50 px-4 py-3 text-sm focus:outline-none focus:ring-2 focus:ring-blue-500 transition-all";
+
+/**
+ * Button Sytles
+ */
+
+export const BUTTON_BASE = "inline-flex items-center justify-center rounded-lg font-bold transition-all duration-200 active:scale-95 disabled:opacity-50 disabled:pointer-events-none cursor-pointer gap-2"
+
+export const BUTTON_VARIANTS = {
+	sm: "px-3 py-1 text-xs",
+	md: "px-6 py-3 text-sm",
+	lg: "px-8 py-4 text-lg",
+}
