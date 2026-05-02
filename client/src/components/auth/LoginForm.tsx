69758ba6db766160246b94f4a3667eb975875fa2
Jesstin Swadley <contact@jesstin.com>
2026-05-01 17:53:38 -0700

    modify LoginForm.tsx to use Input.tsx component

--- client/src/components/auth/LoginForm.tsx
+++ client/src/components/auth/LoginForm.tsx
@@ -4,6 +4,7 @@ import { loginRequest } from '../../services/auth';
 import toast from 'react-hot-toast';
 import { loginSchema, type LoginFormData } from '../../schemas/loginSchema';
 import { useAuth } from '../../context/AuthContext';
+import Input from '../ui/Input';
 
 const LoginForm = () => {
 	const [isLoading, setIsLoading] = useState(false);
@@ -62,107 +63,77 @@ const LoginForm = () => {
 
 	return (
 		<>
-			<form 
+			<form
 				onSubmit={handleSubmit}
-				className="space-y-6">
+	 			className="space-y-6">
+
 					<div
 						className="space-y-1">
-							<h2 className="text-xl font-bold text-black">Login</h2>
-
+							<h2
+								className="text-xl font-bold text-black">
+									Login
+							</h2>
+							
 							<Link
-								className="text-sm font-bold text-gray-300"
-								to="/register">
+								to="/register"
+								className="text-sm font-bold text-gray-300">
 									Need to register an account? Create an account
 							</Link>
 					</div>
-				
-					<div
-						className="space-y-1">
-						<label 
-							className="text-sm font-bold text-black"
-							htmlFor="username">
-								Username
-						</label>
-						<input 
-							type="text" 
-							name="username" 
-							id="username"
-							placeholder='Username'
-							className={`w-full rounded-md border-2 border-gray-200 bg-gray-50 px-4 py-3 text-sm transition-all focus:outline-none focus:ring-2 focus:ring-blue-500 ${
-								fieldErrors.username ? "border-red-500" : "border-gray-200"
-							}`} />
 
-						{fieldErrors.username?.[0] && (
-							<p
-								className="text-xs font-bold text-red-500 transition-opacity duration-300 ease-in opacity-100">
-									{fieldErrors.username[0]}
-							</p>
-						)}
-					</div>
+					<Input
+						type="text"
+						label="Username"
+						id="username"
+						name="username"
+						placeholder="Username"
+						error={fieldErrors.username?.[0]} />
+
+					<Input
+						type="password"
+						label="Password"
+						id="password"
+						name="password"
+						placeholder="Password"
+						error={fieldErrors.password?.[0]} />
 
 					<div
-						className="space-y-1">
-						<label 
-							htmlFor="password"
-							className="text-sm font-bold text-black">
-								Password
-						</label>
-						<input 
-							type="password" 
-							name="password" 
-							id="password"
-							placeholder='Password'
-							className={`w-full rounded-md border-2 border-gray-200 bg-gray-50 px-4 py-3 text-sm transition-all focus:outline-none focus:ring-2 focus:ring-blue-500 ${
-								fieldErrors.password ? "border-red-500" : "border-gray-200"
-							}`} />
-						
-						{fieldErrors.password?.[0] && (
-							<p
-								className="text-xs font-bold text-red-500 transition-opacity duration-300 ease-in opacity-100">
-									{fieldErrors.password[0]}
-							</p>
-						)}
+						className="flex items-center gap-2">
+							<input 
+								type="checkbox"
+								name="remember"
+								id="remember"
+								className="h-5 w-5 rounded border-gray-200 cursor-pointer" />
+
+							<label 
+								htmlFor="remember"
+								className="text-sm font-bold text-black cursor-pointer">
+									Keep me logged in?
+							</label>
 					</div>
 
-				<div
-					className="flex items-center gap-2">
-					<input 
-						type="checkbox" 
-						name="remember" 
-						id="remember" 
-						className="h-5 w-5 rounded border-gray-200" />
-					<label 
-						htmlFor="remember"
-						className="text-sm font-bold text-black" >
-							Keep me logged in?
-					</label>
-				</div>
+					<button
+						type="submit"
+						disabled={isLoading}
+						className={`w-40 rounded-lg py-3 text-lg font-bold text-white transition cursor-pointer ${
+							isLoading ? "bg-gray-400 cursor-wait" : "bg-blue-600 hover:bg-blue-700 shadow-lg shadow-blue-100"
+						}`}>
+							{isLoading ? "Signing in..." : "Login"}
+					</button>
 
-				<button
-					type="submit"
-					disabled={isLoading}
-					className={`w-40 rounded-lg py-3 text-lg font-bold text-white transition cursor-pointer ${
-						isLoading ? "bg-gray-400 cursor-wait" : "bg-blue-600 hover:bg-blue-700"
-					}`}
-				>
-					{isLoading ? "Signing in..." : "Login"}
-				</button>
-
-
-				<div
-					className="flex justify-between text-sm font-bold text-gray-300">
-						<Link
-							className="hover:underline hover:text-blue-500 transition"
-							to="#">
-								Forgot Username?
-						</Link>
-						
-						<Link
-							className="hover:underline hover:text-blue-500 transition"
-							to="#">
-								Forgot Password?
-						</Link>
-				</div>
+					<div
+						className="flex justify-between text-sm font-bold text-gray-300">
+							<Link
+								to="#"
+								className="hover:underline hover:text-blue-500 transition">
+									Forgot Username?
+							</Link>
+							<Link
+								to="#"
+								className="hover:underline hover:text-blue-500 transition">
+									Forgot Password?
+							</Link>
+					</div>
 			</form>
 		</>
 	)
